fcaa98352679022d47a838b3aec5a9811bc7b8af
oscarsangpa <oscarsangrador@gmail.com>
2023-03-05 13:34:04 +0100

    config: eslint and prettier

--- src/db/_connection.js
+++ src/db/_connection.js
@@ -1,16 +1,14 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export function _connect() {
-    const MONGO_HOST = "localhost"
-    const MONGO_DB = "mongo-db"
+	const MONGO_HOST = 'localhost';
+	const MONGO_DB = 'mongo-db';
 
-    const URI = `mongodb://${MONGO_HOST}/${MONGO_DB}`
+	const URI = `mongodb://${MONGO_HOST}/${MONGO_DB}`;
 
-    mongoose.connect(URI,
-        {useNewUrlParser: true},
-        {useUnifiedTopology: true}
-        )
+	mongoose
+		.connect(URI, { useNewUrlParser: true }, { useUnifiedTopology: true })
 
-        .then(db => console.log("Connection ready to use", db.connection.port))
-        .catch(err => console.log(err))
+		.then(db => console.log('Connection ready to use', db.connection.port))
+		.catch(err => console.log(err));
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import express from "express";
-import { _connect } from "./db/_connection.js";
+import express from 'express';
+import { _connect } from './db/_connection.js';
 
 _connect();
 const app = express();
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`listen on PORT ${PORT}`);
+    console.log(`listen on PORT ${PORT}`);
 });
+
+
