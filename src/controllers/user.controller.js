041890d8ba0a9c82b5ef532fc60e859ea106f761
oscarsangpa <oscarsangrador@gmail.com>
2023-03-06 13:41:59 +0100

    feat: create user token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from '../models/User.model.js'
+import { idGenerator } from '../helpers/idGenerator.js'
 
 const register = async (req, res) => {
     // avoid duplicate register
@@ -9,9 +10,10 @@ const register = async (req, res) => {
         const error = new Error("Email already exist")
         return res.status(400).json({ msg: error.message })
     }
-    
+
     try {
         const user = new User(req.body)
+        user.token = idGenerator()
         const newUser = await user.save()
         res.json(newUser)
     } catch (error) {
