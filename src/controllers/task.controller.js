c12dfc043b8034b41bc1671c0c1f5ec5c17f14be
oscarsangpa <oscarsangrador@gmail.com>
2023-03-22 10:55:17 +0100

    feat: endpoint to delete task

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -70,7 +70,29 @@ const editTask = async (req, res) => {
     }
 }
 
-const deleteTask = async () => { }
+const deleteTask = async (req, res) => {
+    const { id } = req.params
+
+    const task = await Task.findById(id).populate('project')
+
+    if (!task) {
+        const error = new Error('Task not found')
+        return res.status(404).json({ msg: error.message })
+    }
+
+    if (task.project.creator.toString() !== req.user._id.toString()) {
+        const error = new Error('Invalid action')
+        res.status(403).json({ msg: error.message })
+    }
+
+    try {
+        await task.deleteOne()
+        res.json({ msg: 'Delete Task Correctly' })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 const changeStateTask = async () => { }
 
 export {
