f50b399906143429e12c563d027bb986f57074f8
oscarsangpa <oscarsangrador@gmail.com>
2023-03-22 09:31:34 +0100

    feat: endpoint to edit task

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -41,7 +41,35 @@ const addTask = async (req, res) => {
         console.error(error)
     }
 }
-const editTask = async () => { }
+
+const editTask = async (req, res) => {
+    const { id } = req.params
+
+    const task = await Task.findById(id).populate('project')
+
+    if (!task) {
+        const error = new Error('Task not found')
+        return res.status(404).json({ msg: error.message })
+    }
+
+    if (task.project.creator.toString() !== req.user._id.toString()) {
+        const error = new Error('Invalid action')
+        res.status(403).json({ msg: error.message })
+    }
+
+    task.name = req.body.name || task.name
+    task.description = req.body.description || task.description
+    task.proiority = req.body.proiority || task.proiority
+    task.deadLine = req.body.deadLine || task.deadLine
+
+    try {
+        const editTask = await task.save()
+        res.json(editTask)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
 const deleteTask = async () => { }
 const changeStateTask = async () => { }
 
